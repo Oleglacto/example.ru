28308f71c386b88e62ea934b6f71b714a1b9fe97
Oleg Lactyushkin <oleglacto@gmail.com>
2017-07-31 12:28:00 +0300

    Create first page

--- Views/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: oleg
- * Date: 31.07.17
- * Time: 11:14
- */
--- www/index.php
+++ www/index.php
@@ -2,11 +2,18 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Тестовая страница</title>
+      <link href="/styles/normalize.min.css" rel="stylesheet" type="text/css">
+      <link href="/styles/style.css" rel="stylesheet" type="text/css">
   </head>
   <body>
-    Тестовая страница1
-    <?php
-  phpinfo();
-?>
+  <header class="main-header">
+      <?php require(__DIR__ . "/../views/header.php");?>
+  </header>
+  <main class="container">
+        <?php require (__DIR__.'/../views/content.php');?>
+  </main>
+  <footer class="main-footer">
+      <?php require (__DIR__.'/../views/footer.php'); ?>
+  </footer>
   </body>
 </html>
