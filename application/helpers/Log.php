b1f55c4d62f73c611f8eabb1400e8877ce431a33
Oleg Lactyushkin <oleglacto@gmail.com>
2017-11-23 22:58:23 +0300

    класс для логирования ошибок

---
+++ application/helpers/Log.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oleg
+ * Date: 23.11.17
+ * Time: 22:46
+ */
+
+namespace application\helpers;
+
+
+class Log
+{
+    /**
+     * расположение файла error.log
+     * @var $file
+     */
+    protected $file = __DIR__ . '/../../logs/error.log';
+
+    /**
+     * Сообщение ошибки
+     * @var $message
+     */
+    protected $message;
+
+    /**
+     * Время ошибки
+     * @var $time
+     */
+    protected $time;
+
+    public function __construct($message)
+    {
+        $this->time = date("H:i:s");
+        $this->message = $this->time . " ";
+        $this->message .= $message . '\n';
+        file_put_contents($this->file, $this->message, FILE_APPEND | LOCK_EX);
+    }
+}
