acc09cc9e80032d03b75b59eb6bacbf956e97361
Oleg <oleglacto@gmail.com>
2017-11-29 20:23:36 +0300

    added errorHandler and set errorHandler as default

--- www/index.php
+++ www/index.php
@@ -3,11 +3,14 @@
 ini_set('display_errors', 1);
 
 
-
+function errorHandler($exception){
+    $log = new \application\helpers\Log();
+    $log->writeLog($exception->getMessage());
+}
 function my_autoload($className)
 {
-    $className = str_replace('\\',DIRECTORY_SEPARATOR,$className);
-    $file = __DIR__.'/..'.DIRECTORY_SEPARATOR. $className . '.php';
+    $className = str_replace('\\',DIRECTORY_SEPARATOR, $className);
+    $file = __DIR__.'/..'.DIRECTORY_SEPARATOR . $className . '.php';
     if(file_exists($file)){
         require_once $file;
     }
@@ -16,6 +19,8 @@ function my_autoload($className)
 
 // автозагрузка классов
 spl_autoload_register('my_autoload');
+//обработчик ошибок
+set_exception_handler('errorHandler');
 require_once '../application/bootstrap.php';
 
 
