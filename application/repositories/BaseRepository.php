488aa4f0316a1d22c4ed6ba60b54be76b8d9efa1
Oleg <oleglacto@gmail.com>
2018-02-10 13:30:37 +0300

    small changes

--- application/repositories/BaseRepository.php
+++ application/repositories/BaseRepository.php
@@ -24,11 +24,37 @@ abstract class BaseRepository
 
     public function __construct()
     {
-        $this->connection = new Database();
+        $this->connection = Database::getInstance();
+    }
+
+    /**
+     * Возвращает массив с заданными параметрами
+     * @param array $parameters (example 'where $parameters')
+     * @param string|null $columns (example 'id,name')
+     * @return array
+     */
+    public function get(array $parameters, $columns = null)
+    {
+        $dataToExecute = $this->connection->getPreparedData($parameters);
+        foreach ($dataToExecute['columns'] as $key => $value) {
+            $columnAndAnchor[] = $value . ' = ' . $dataToExecute['anchors'][$key];
+        }
+        $columnAndAnchor = implode(' and ', $columnAndAnchor);
+        if ($columns) {
+            $sql = "SELECT $columns FROM $this->table WHERE $columnAndAnchor";
+        } else {
+            $sql = "SELECT * FROM $this->table WHERE $columnAndAnchor";
+        }
+
+        if($result = $this->connection->makeSelect($sql,[$dataToExecute['values'],$dataToExecute['anchors']])) {
+            return $result;
+        }
+        return [];
     }
 
     /**
      * Изменение записи в бд
+     * @param $id
      * @param $data
      */
     public function edit($id, $data)
@@ -48,14 +74,16 @@ abstract class BaseRepository
     /**
      * Добавление записи в таблицу
      * @param $data
+     * @return bool
      */
     public function add($data)
     {
         $dataToExecute = $this->connection->getPreparedData($data);
-        $anchors = implode(',',$dataToExecute['anchors']);
-        $columns = implode(',',$dataToExecute['columns']);
+        $anchors = implode(',', $dataToExecute['anchors']);
+        $columns = implode(',', $dataToExecute['columns']);
         $sql = "INSERT INTO $this->table (". $columns . ") VALUES (" . $anchors . ')';
-        $this->runQuery($sql, [$dataToExecute['values'], $dataToExecute['anchors']]);
+        $result = $this->runQuery($sql, [$dataToExecute['values'], $dataToExecute['anchors']]);
+        return $result;
     }
 
     /**
@@ -73,10 +101,15 @@ abstract class BaseRepository
      * Удаление из таблицы элемента по id
      * @param $id
      */
-    public function deleteById($id)
+    public function delete($id)
     {
         $sql = "DELETE FROM $this->table WHERE id = :id";
         $this->runQuery($sql,[[$id], [':id']]);
+    }
+
+    public function getError()
+    {
+        return $this->connection->getError();
     }
 
     /**
@@ -84,12 +117,16 @@ abstract class BaseRepository
      * Если запрос неудачный, показывает ошибку
      * @param $sql
      * @param null|array $values
+     * @return bool
      */
     protected function runQuery($sql, $values = null)
     {
             if(!$this->connection->executeQuery($sql, $values)) {
-               var_dump($this->connection->getError());
+               //var_dump($this->connection->getError());
+               return false;
             }
+            return true;
     }
+
 
 }
--- application/repositories/CakeRepository.php
+++ application/repositories/CakeRepository.php
@@ -11,30 +11,4 @@ class CakeRepository extends BaseRepository {
 
     protected $table = 'cakes';
 
-    /**
-     * Вот тут ниже разве не универсальный запрос? Везде надо делать запрсоы с условием where,
-     * мб вынести в базовый класс подобные методы, и сделать чтоб они точно универсальные были?
-     * А репозитории кастомные будут хранить сложный sql, запросы сложнее селекта
-     */
-
-    /**
-     * Возвращает массив с заданными параметрами
-     * @param array $parameters (example 'where $parameters')
-     * @param string|null $columns (example 'id,name')
-     * @return array|bool
-     */
-    public function getCakes($parameters, $columns = null)
-    {
-        $dataToExecute = $this->connection->getPreparedData($parameters);
-        foreach ($dataToExecute['columns'] as $key => $value) {
-            $columnAndAnchor[] = $value . ' = ' . $dataToExecute['anchors'][$key];
-        }
-        $columnAndAnchor = implode(' and ',$columnAndAnchor);
-        if ($columns) {
-            $sql = "SELECT $columns FROM $this->table WHERE $columnAndAnchor";
-        } else {
-            $sql = "SELECT * FROM $this->table WHERE $columnAndAnchor";
-        }
-        return $this->connection->makeSelect($sql,[$dataToExecute['values'],$dataToExecute['anchors']]);
-    }
 }
